880fbe782fdd2855f8c7f78747510e81eeb67db1
boroviack <132918132+boroviack@users.noreply.github.com>
2024-09-03 11:13:06 -0300

    kkkkkkkk

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-const elemento = document.querySelector(".teste");
+const caixaPrincipal = document.querySelectorAll(".caixa-principal")
+const caixaPergunta = document.querySelectorAll (".caixa-perguntas")
+const caixaAlternativas = document.querySelectorAll (".caixa-alternativas")
+const caixaResultado = document.querySelectorAll (".caixa-resultado")
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Qual a data da independencia do Brasil?",
+        alternativas: ["7 de novembro","25 de dezembro","7 de setembro"]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+perguntaAtual = perguntas [atual];
+caixaPergunta.textContent = perguntaAtual.enunciado;
+    
+}
+
+mostraPergunta();
